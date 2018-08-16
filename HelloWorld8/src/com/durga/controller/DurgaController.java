2d6a5b34f819894c9687ab6e2ef7301e832604af
durgareddy577 <durgareddy577.java@gmail.com>
2018-08-16 18:45:29 +0530

    new sop is added

--- HelloWorld8/src/com/durga/controller/DurgaController.java
+++ HelloWorld8/src/com/durga/controller/DurgaController.java
@@ -8,5 +8,6 @@ public class DurgaController
     	 System.out.println("hello2");
     	 System.out.println("hello3");
     	 System.out.println("hello4");
+    	 System.out.println("hello5");
      }
 }
